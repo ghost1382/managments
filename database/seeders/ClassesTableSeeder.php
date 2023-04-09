7068935ad1df845a9b6814b8a83be520e47a8640
ghost1382 <mohammed.bn884@gmail.com>
2023-04-09 16:20:29 +0000

    change the seeder

--- database/seeders/ClassesTableSeeder.php
+++ database/seeders/ClassesTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ClassNameSeeder extends Seeder
+class ClassesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
